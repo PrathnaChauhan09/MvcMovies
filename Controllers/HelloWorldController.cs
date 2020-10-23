4102dd18680bf30e50c16579dfbab748e2f5a25f
PrathnaChauhan09 <prathna@promactinfo.com>
2020-10-23 12:56:52 +0530

    adding files

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovies.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //Get : /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        //Get : /HelloWorld/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["message"] = "Hello " + name+",";
+            ViewData["numTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
